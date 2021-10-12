a897c297f9d4a34f8c0480e49bca2a5faefdedf5
richardreiter <richard.reiter.dev@gmail.com>
2021-10-12 13:08:15 +0000

    Add function to get computers choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // array of options index 0 is rock, index 1 is paper and index 2 is scissors
 const options = ["rock", "paper", "scissors"];
 
+// function to get computers choice
+const computersChoice = () => {
+    //generate a random number between 0-2 (represents index 0 rock/index 1 paper/index 2 scissors in numerals, depending on the random number)
+    const randomNumber = Math.floor(Math.random() * 3);
+    const computerOption = options[randomNumber];
+    return computerOption;
+};
+
 // event listeners for the players choice, get span element by its id when player makes a choice and change the inner html of the span
 const rockChoice = document.getElementById("rock").addEventListener("click", function() {
     document.getElementById("yourChoice").innerHTML = "Rock ✊";
