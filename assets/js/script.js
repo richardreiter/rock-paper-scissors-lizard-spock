c08e83fddfdce0a38cfaea00c2a175857e905d66
richardreiter <richard.reiter.dev@gmail.com>
2021-10-12 11:28:15 +0000

    Add an array of numeral/index options for rock/paper/scissors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// array of options index 0 is rock, index 1 is paper and index 2 is scissors
+const options = ["rock", "paper", "scissors"];
+
 // event listeners for the players choice, get span element by its id when player makes a choice and change the inner html of the span
 const rockChoice = document.getElementById("rock").addEventListener("click", function() {
     document.getElementById("yourChoice").innerHTML = "Rock ✊";
